5f802243c96d15a43323de36584e36861caa076c
B. Gunnarsson <dentadesign@gmail.com>
2026-09-22 20:48:55 +0000

    binvim 0.6.6

--- Formula/binvim.rb
+++ Formula/binvim.rb
@@ -1,8 +1,8 @@
 class Binvim < Formula
   desc "Vim-grammar TUI editor with batteries included"
   homepage "https://github.com/bgunnarsson/binvim"
-  url "https://github.com/bgunnarsson/binvim/archive/refs/tags/v0.6.5.tar.gz"
-  sha256 "2c1888b94ba58891dda7ba1fafabcfab5ebec86aa09bfa1972b194a5cc52a64b"
+  url "https://github.com/bgunnarsson/binvim/archive/refs/tags/v0.6.6.tar.gz"
+  sha256 "74dadb501956530775b7ce9332ff20128da9bee09abf8107ce6abab67d98eae6"
   license :cannot_represent
 
   depends_on "rust" => :build
